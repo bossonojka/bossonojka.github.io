bda0886afdaa816da0ab6411a3a1a342d820b67d
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-02 11:11:21 +0300

    Creating sw

---
+++ img/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install',function(e){
+    e.waitUntil(
+        caches.open('images-store').then(function(cache){
+            console.log('Filling cache...');
+            return cache.addAll([
+                '/img/',
+                '/img/index.html',
+                '/img/main.css',
+                '/img/main.js',
+                '/img/gallery.js',
+                'img/images/',
+                'img/images/Nature_1.jpg',
+                'img/images/Nature_2.jpg',
+                'img/images/Nature_3.jpg'
+            ]);
+        })
+    );
+});
