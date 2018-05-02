ec9e019ee66f11a9bc57f4e08ab206eb70ee8381
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-02 10:46:49 +0300

    Initial commit

---
+++ img/gallery.js
@@ -0,0 +1,12 @@
+var Gallery = {images: [
+        {name  : 'Forest',
+        alt : 'Forest',
+        url: 'images/Nature_1.jpg',},
+        {name  : 'Mountains',
+        alt : 'Mountains',
+        url: 'images/Nature_2.jpg',},
+        {name  : 'River',
+        alt : 'River',
+        url: 'images/Nature_3.jpg',}
+    ]
+}
---
+++ img/main.js
@@ -0,0 +1,52 @@
+function imgLoad(obj){
+    return new Promise(function(resolve, reject){
+        var request = new XMLHttpRequest();
+        request.open('GET', obj.url);
+        request.responseType = 'blob';
+        request.send();
+
+        request.onload = function(){
+            if (request.status == 200){
+                let responseArr = [];
+                responseArr[0] = request.response;
+                responseArr[1] = obj;
+                resolve(responseArr);
+                console.log(request.response);
+            } else {
+                reject(request.statusText);
+            }
+        }
+
+        request.onerror = function(){
+            console.error('Network error.');
+        }
+    });
+}
+
+let section = document.querySelector('section');
+
+for (var i = 0; i < Gallery.images.length; i++){
+    imgLoad(Gallery.images[i]).then(function(response){
+        let img = new Image();
+        let figure = document.createElement('figure');
+        let caption = document.createElement('caption')
+
+        img.src = URL.createObjectURL(response[0]);
+        caption.innerHTML = '<strong>' + response[1].name + '</strong>';
+
+        figure.appendChild(img);
+        figure.appendChild(caption);
+        section.appendChild(figure);
+
+    }, function(error){
+        console.error(error);
+    })
+}
+
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/img/sw.js').then(function(reg){
+        console.log('Registration succeeded. ' + reg);
+    }).catch(function(error) {
+        console.log('Registration failed with ' + error);
+      });
+}
