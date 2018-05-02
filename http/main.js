f443287b702de2ab0460490b25da6d2860a5ad9f
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-02 16:10:00 +0300

    Improvements

--- http/main.js
+++ http/main.js
@@ -1,14 +1,27 @@
-function showImg(url){
+function showImg(obj){
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+    xhr.open('GET', obj.url);
     xhr.responseType = 'blob';
     xhr.send();
 
     xhr.onload = function(){
         if (xhr.status == 200){
+            let responseArr = [];
+            responseArr[0] = xhr.response;
+            responseArr[1] = obj;
+            console.log(xhr.responseURL);
+
             let img = new Image();
-            img.src = URL.createObjectURL(xhr.response);
-            document.body.appendChild(img);
+            let figure = document.createElement('figure');
+            let caption = document.createElement('caption');
+            let section = document.querySelector('section');
+
+            img.src = URL.createObjectURL(responseArr[0]);
+            caption.innerHTML = '<strong>' + responseArr[1].name + '</strong>';
+
+            figure.appendChild(img);
+            figure.appendChild(caption);
+            section.appendChild(figure);
         } else {
             console.log(xhr.statusText);
         }
@@ -19,9 +32,17 @@ function showImg(url){
     }
 }
 
-let images = ['images/Nature_1.jpg','images/Nature_2.jpg','images/Nature_3.jpg'];
+let gallery = {images: [
+    {name: "Forest",
+    url: 'images/Nature_1.jpg'},
+    {name: "Mountains",
+    url: 'images/Nature_2.jpg'},
+    {name: "River",
+    url: 'images/Nature_3.jpg'}
+]};
 
-for (var i = 0; i < images.length; i++){
-    showImg(images[i]);
+for (var i = 0; i < gallery.images.length; i++){
+    showImg(gallery.images[i]);
+    console.log("Fetching image " + i + "...");
 }
 
