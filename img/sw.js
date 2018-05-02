cc7eb5c556a49645923de462a3ce6206f9ff9561
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-02 21:09:27 +0300

    Fixing fetch

--- img/sw.js
+++ img/sw.js
@@ -20,7 +20,13 @@ self.addEventListener('fetch', function(e){
     e.respondWith(
         caches.match(e.request).then(function(response){
             console.log(response);
-            return response || fetch(e.request);
+            return response || fetch(e.request).then(function(response){
+                return caches.open('img-store').then(function(cache){
+                    cache.put(e.request, response.clone());
+                    console.log(response);
+                    return response;
+                })
+            });
         })
     );
 })
