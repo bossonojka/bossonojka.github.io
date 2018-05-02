dcfa4911e812d594be02f6c0cf57080662334c24
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-02 11:34:16 +0300

    Revert "Fixing paths"
    This reverts commit 0debcc2c36453f5c11de3d286785ae72be3ac803.

--- img/sw.js
+++ img/sw.js
@@ -8,6 +8,7 @@ self.addEventListener('install',function(){
                 '/img/main.css',
                 '/img/main.js',
                 '/img/gallery.js',
+                '/img/images/',
                 '/img/images/Nature_1.jpg',
                 '/img/images/Nature_2.jpg',
                 '/img/images/Nature_3.jpg'
