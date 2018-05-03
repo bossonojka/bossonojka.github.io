740fb0eb384956116a9d1fcd0ec4ede2c640ccc4
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-03 16:34:31 +0300

    Enhanced cache

--- notes/sw.js
+++ notes/sw.js
@@ -11,12 +11,30 @@ self.addEventListener('install', function(e) {
    })
  );
 });
+
+self.addEventListener('activate', function(e){
+  console.info("Activate event...");
+
+  let trueCache = ['notes-store'];
+
+  e.waitUntil(
+      caches.keys().then(keyList => {
+          keyList.map(key =>{
+              if (trueCache.indexOf(key) === -1){
+                  console.log("Deleted key: " + key);
+                  caches.delete(key);
+              }
+          })
+      })
+  );
+})
+
 self.addEventListener('fetch',function(e){
     console.log(e);
     console.log(e.request.url);
-    e.respondWith(caches.match(e.request).then(function(response){
-        return response || fetch(e.request).then(function(response){
-          return caches.open('notes-store').then(function(cache){
+    e.respondWith(caches.match(e.request).then(function(res){
+        return res || fetch(e.request).then(function(response){
+          caches.open('notes-store').then(function(cache){
             cache.put(e.request, response.clone());
             return response;
           })
