f0a45d6f229039d5c5dd3654cc6d0d042fea3599
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-03 12:41:40 +0300

    Fixing returns

--- img/sw.js
+++ img/sw.js
@@ -16,7 +16,7 @@ self.addEventListener('fetch', function(e){
     e.respondWith(
         caches.match(e.request).then(function(response){
             return response || fetch(e.request).then(function(response){
-                return caches.open('img-store').then(function(cache){
+                caches.open('img-store').then(function(cache){
                     cache.put(e.request, response.clone());
                     return response;
                 })
