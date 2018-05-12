bd41033f6d0729f84325b59fb39ff71a884dfe74
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-12 18:49:09 +0300

    activate event added

--- jQuery/sw.js
+++ jQuery/sw.js
@@ -2,10 +2,26 @@ self.addEventListener('install', function(e){
     e.waitUntil(
         caches.open('ver1')
         .then(cache => {
-            return cache.addAll([
+            cache.addAll([
                 '/jQuery/index.html',
                 '/jQuery/sw.js'
             ])
+        })
+    );
+})
+
+self.addEventListener('activate', function(e){
+    let myCache = ['ver1'];
+    
+    e.waitUntil(
+        caches.keys()
+        .then(cacheList => {
+            cacheList.map(function(cache){
+                if(myCache.indexOf(cache) === -1){
+                    console.log('Deleted cache: ' + cache);
+                    caches.delete(cache);
+                }
+            });
         })
     );
 })
