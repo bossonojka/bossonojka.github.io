e321ab23bd55677a2a266fb1344ba49ee2b6bffa
Bossonojka <36764653+bossonojka@users.noreply.github.com>
2018-05-12 19:00:48 +0300

    added return value

--- jQuery/sw.js
+++ jQuery/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e){
     e.waitUntil(
         caches.open('ver1')
         .then(cache => {
-            cache.addAll([
+            return cache.addAll([
                 '/jQuery/index.html',
                 '/jQuery/sw.js'
             ])
