fdfd240635a9f328ba8a7ead459b78800fe649eb
Bossonojka <bossonojka@gmail.com>
2019-05-04 15:22:11 +0300

    Date of the note has been added. Now videos can be uploaded.

--- notes/index-start.js
+++ notes/index-start.js
@@ -18,8 +18,13 @@ const closeBtn = document.querySelector("#closeBtn");
 const imgPreview = document.querySelector(".imgPreview");
 const imgPreviewEdit = document.querySelector(".imgPreviewEdit");
 
+const videoPreview = document.querySelector(".videoPreview");
+const videoPreviewEdit = document.querySelector(".videoPreviewEdit");
+
 let db;
 let noteId;
+
+let tempVideo, tempImg = "";
 
 window.onload = function(){
     let request = window.indexedDB.open("notes", 1);
@@ -41,7 +46,9 @@ window.onload = function(){
 
         objectStore.createIndex("title", "title", {unique: false});
         objectStore.createIndex("body", "body", {unique: false});
+        objectStore.createIndex("date", "date", {unique: false});
         objectStore.createIndex("img", "img", {unique: false});
+        objectStore.createIndex("video", "video", {unique: false});
 
         console.log("Database setup complete");
     }
@@ -66,16 +73,20 @@ function setControlBtn(){
     }
 
     attachInput.onchange = function(e){
-        displayPreview(e, imgPreview, attachBtn);
+        cleanPreviews();
+        let file = e.target.files[0];
+        (e.target.files[0].type != "video/mp4") ? displayPreviewImg(file, imgPreview, attachBtn) : displayPreviewVideo(file, videoPreview, attachBtn);
     }
 
-    attachBtnEdit.onclick = function(e){
+    attachBtnEdit.onclick = function(e){ 
         e.preventDefault();
         attachInputEdit.click();     
     }
 
     attachInputEdit.onchange = function(e){
-        displayPreview(e, imgPreviewEdit, attachBtnEdit);
+        cleanPreviews();
+        let file = e.target.files[0];
+        (e.target.files[0].type != "video/mp4") ? displayPreviewImg(file, imgPreviewEdit, attachBtnEdit) : displayPreviewVideo(file, videoPreviewEdit, attachBtnEdit);
     }
 
     closeBtn.onclick = function(e){
@@ -84,16 +95,44 @@ function setControlBtn(){
     }
 }
 
-function displayPreview(e, yourImg, nearBtn){
-    let blob = URL.createObjectURL(e.target.files[0]);
+function cleanPreviews(){
+    imgPreview.src = imgPreviewEdit.src = "";
+    videoPreview.src = videoPreviewEdit.src = "";
+}
+
+function displayPreviewImg(file, yourImg, nearBtn){
+    let blob = URL.createObjectURL(file);
     yourImg.src = blob;
     yourImg.height = nearBtn.getBoundingClientRect().height;
+}
+
+function displayPreviewVideo(file, yourVideo, nearBtn){
+    let blob = URL.createObjectURL(file);
+    yourVideo.src = blob;
+    yourVideo.height = nearBtn.getBoundingClientRect().height;
+    yourVideo.hidden = false;
 }
 
 function addData(e){
     e.preventDefault();
 
-    let newItem = {title: title.value, body: body.value, img: attachInput.files[0]};
+    videoPreview.hidden = true;
+
+    let today = new Date();
+    let date = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
+
+    let video = "";
+    let img = "";
+
+    if (attachInput.files.length != 0){
+        if (attachInput.files[0].type != "video/mp4"){
+            img = attachInput.files[0];
+        } else {
+            video = attachInput.files[0];
+        }
+    }
+
+    let newItem = {title: title.value, body: body.value, date: date, img: img, video: video};
 
     let transaction = db.transaction(["notes"], "readwrite");
 
@@ -104,6 +143,7 @@ function addData(e){
     request.onsuccess = function(){
         title.value = "";
         body.value = "";
+        date.value = "";
         attachInput.value = "";
         imgPreview.src = "";
     }
@@ -132,6 +172,7 @@ function displayData(){
             let listItem = document.createElement('li');
             let h3 = document.createElement('h3');
             let para = document.createElement('p');
+            let dateItem = document.createElement('span');
             
             listItem.appendChild(h3);
             listItem.appendChild(para);
@@ -140,11 +181,19 @@ function displayData(){
             h3.textContent = cursor.value.title;
             para.textContent = cursor.value.body;
             
+            h3.appendChild(dateItem);
+            dateItem.textContent = cursor.value.date;
             if (cursor.value.img){
                 let img = document.createElement("img");
                 img.src = URL.createObjectURL(cursor.value.img);
                 img.className = "imgNote";
                 listItem.appendChild(img);
+            } else if (cursor.value.video) {
+                let video = document.createElement("video");
+                video.src = URL.createObjectURL(cursor.value.video);
+                video.className = "imgNote";
+                video.controls = true;
+                listItem.appendChild(video);
             }
 
             listItem.setAttribute("data-note-id",cursor.value.id);
@@ -166,6 +215,26 @@ function displayData(){
             editBtn.onclick = function(e){
                 noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
                 sectionEdit.hidden = false;
+
+                attachInputEdit.form.reset();
+                imgPreviewEdit.src = "";
+                videoPreviewEdit.src = "";
+
+                let result = getNoteData(noteId)
+                result.onsuccess = function(e) {
+                    titleNew.value = e.target.result.title;
+                    bodyNew.value = e.target.result.body;
+                   
+                    if (e.target.result.img != ""){
+                        tempImg = e.target.result.img;
+                        displayPreviewImg(e.target.result.img, imgPreviewEdit, attachBtn);
+                    }
+
+                    if (e.target.result.video != ""){
+                        tempVideo = e.target.result.video;
+                        displayPreviewVideo(e.target.result.video, videoPreviewEdit, attachBtn);
+                    }
+                };
             };
 
             cursor.continue();
@@ -173,7 +242,7 @@ function displayData(){
             
             if (!list.firstChild) {
               let listItem = document.createElement('li');
-              listItem.textContent = 'No notes stored in IndexDB';
+              listItem.textContent = 'No notes stored in database.';
               list.appendChild(listItem);
             }
         }
@@ -192,31 +261,70 @@ function deleteItem(e){
         console.log('Note ' + noteId + ' deleted.');
         if (!list.firstChild) {
             let listItem = document.createElement('li');
-            listItem.textContent = 'No notes stored in IndexDB.';
+            listItem.textContent = 'No notes stored in database.';
             list.appendChild(listItem);
         }
     }
 }
 
+function getNoteData(id){
+    let transaction = db.transaction(["notes"]);
+    let objectStore = transaction.objectStore("notes");
+    
+    let objectStoreRequest = objectStore.get(id);
+
+    return objectStoreRequest;
+}
+
 function editItem(e){
     e.preventDefault();
 
-    let newItem = {title: titleNew.value, body: bodyNew.value, img: attachInputEdit.files[0], id:noteId};
+    let today = new Date();
+    let date = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
 
-    let transaction = db.transaction(["notes"],"readwrite");
-    let objectStore = transaction.objectStore("notes");
-    let request = objectStore.put(newItem);
+    let video = "";
+    let img = "";
 
+    //let newItem;
 
-    transaction.oncomplete = function(){
-        console.log('Transaction completed: database modification finished.');
-        
-        titleNew.value = "";
-        bodyNew.value = "";
-        attachInputEdit.value = "";
-        imgPreviewEdit.src = "";
-        sectionEdit.hidden = true;
-        
-        displayData();
+    let note = getNoteData(noteId)
+    note.onsuccess = function(e) {
+        if (attachInputEdit.files.length != 0){
+            if (attachInputEdit.files[0].type != "video/mp4"){
+                img = attachInputEdit.files[0];
+                console.log(img);
+            } else {
+                video = attachInputEdit.files[0];
+                console.log(video);
+            }
+        } else {
+            if (e.target.result.img != ""){
+                img = e.target.result.img;
+                console.log(img);
+            } else {
+                video = e.target.result.video;
+                console.log(video);
+            }
+        }
+
+        let newItem = {title: titleNew.value, body: bodyNew.value, date: date, img: img, video: video, id: noteId};
+
+        let transaction = db.transaction(["notes"],"readwrite");
+        let objectStore = transaction.objectStore("notes");
+        let request = objectStore.put(newItem);
+    
+        transaction.oncomplete = function(){
+            console.log('Transaction completed: database modification finished.');
+            
+            titleNew.value = "";
+            bodyNew.value = "";
+            attachInputEdit.value = "";
+            imgPreviewEdit.src = "";
+            sectionEdit.hidden = true;
+    
+            tempImg = tempVideo = "";
+            
+            displayData();
+        }
     }
 }
