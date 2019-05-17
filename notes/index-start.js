7f42b5fe9bc92539533600dcfa7715d4240d0e52
Bossonojka <bossonojka@gmail.com>
2019-05-17 17:19:15 +0300

    Grouping by date was added

--- notes/index-start.js
+++ notes/index-start.js
@@ -21,6 +21,8 @@ const imgPreviewEdit = document.querySelector(".imgPreviewEdit");
 const videoPreview = document.querySelector(".videoPreview");
 const videoPreviewEdit = document.querySelector(".videoPreviewEdit");
 
+const datePicker = document.querySelector("#datePicker");
+
 let db;
 let noteId;
 
@@ -117,10 +119,8 @@ function addData(e){
     e.preventDefault();
 
     videoPreview.hidden = true;
-
-    let today = new Date();
-    let date = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
-
+    
+    let date = new Date();
     let video = "";
     let img = "";
 
@@ -169,6 +169,7 @@ function displayData(){
         let cursor = e.target.result;
 
         if (cursor){
+
             let listItem = document.createElement('li');
             let h3 = document.createElement('h3');
             let para = document.createElement('p');
@@ -182,7 +183,9 @@ function displayData(){
             para.textContent = cursor.value.body;
             
             h3.appendChild(dateItem);
-            dateItem.textContent = cursor.value.date;
+            let today = cursor.value.date;
+            dateItem.textContent = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
+            
             if (cursor.value.img){
                 let img = document.createElement("img");
                 img.src = URL.createObjectURL(cursor.value.img);
@@ -249,6 +252,117 @@ function displayData(){
     }
 }
 
+function displayDataByDate(date){
+    let objectStore = db.transaction('notes').objectStore('notes');
+
+    while (list.firstChild){
+        list.removeChild(list.firstChild);
+    }
+    
+    objectStore.openCursor().onsuccess = function(e){
+        let cursor = e.target.result;
+        if (cursor){
+            if (cursor.value.date.toDateString() == date.toDateString()){
+                let listItem = document.createElement('li');
+                let h3 = document.createElement('h3');
+                let para = document.createElement('p');
+                let dateItem = document.createElement('span');
+                
+                listItem.appendChild(h3);
+                listItem.appendChild(para);
+                list.appendChild(listItem);
+
+                h3.textContent = cursor.value.title;
+                para.textContent = cursor.value.body;
+                
+                h3.appendChild(dateItem);
+                let today = cursor.value.date
+                dateItem.textContent = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
+                if (cursor.value.img){
+                    let img = document.createElement("img");
+                    img.src = URL.createObjectURL(cursor.value.img);
+                    img.className = "imgNote";
+                    listItem.appendChild(img);
+                } else if (cursor.value.video) {
+                    let video = document.createElement("video");
+                    video.src = URL.createObjectURL(cursor.value.video);
+                    video.className = "imgNote";
+                    video.controls = true;
+                    listItem.appendChild(video);
+                }
+
+                listItem.setAttribute("data-note-id",cursor.value.id);
+
+                let deleteBtn = document.createElement("button");
+
+                deleteBtn.className = "secControls";
+                deleteBtn.textContent = "Delete";
+                listItem.appendChild(deleteBtn);
+
+                deleteBtn.onclick = deleteItem;
+
+                let editBtn = document.createElement("button");
+
+                editBtn.className = "secControls";
+                editBtn.textContent = "Edit";
+                listItem.appendChild(editBtn);
+
+                editBtn.onclick = function(e){
+                    noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
+                    sectionEdit.hidden = false;
+
+                    attachInputEdit.form.reset();
+                    imgPreviewEdit.src = "";
+                    videoPreviewEdit.src = "";
+
+                    let result = getNoteData(noteId)
+                    result.onsuccess = function(e) {
+                        titleNew.value = e.target.result.title;
+                        bodyNew.value = e.target.result.body;
+                    
+                        if (e.target.result.img != ""){
+                            tempImg = e.target.result.img;
+                            displayPreviewImg(e.target.result.img, imgPreviewEdit, attachBtn);
+                        }
+
+                        if (e.target.result.video != ""){
+                            tempVideo = e.target.result.video;
+                            displayPreviewVideo(e.target.result.video, videoPreviewEdit, attachBtn);
+                        }
+                    };
+                };
+            }
+
+            cursor.continue();
+        } else {
+            
+            if (!list.firstChild) {
+              let listItem = document.createElement('li');
+              listItem.textContent = 'No notes stored in database.';
+              list.appendChild(listItem);
+            }
+        }
+    }
+}
+
+function deleteItem(e){
+    let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
+
+    let transaction = db.transaction(['notes'], 'readwrite');
+    let objectStore = transaction.objectStore('notes');
+    let request = objectStore.delete(noteId);
+
+    transaction.oncomplete = function(){
+        e.target.parentNode.remove();
+        console.log('Note ' + noteId + ' deleted.');
+        if (!list.firstChild) {
+            let listItem = document.createElement('li');
+            listItem.textContent = 'No notes stored in database.';
+            list.appendChild(listItem);
+        }
+    }
+}
+
 function deleteItem(e){
     let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
 
@@ -279,8 +393,7 @@ function getNoteData(id){
 function editItem(e){
     e.preventDefault();
 
-    let today = new Date();
-    let date = today.getDate() + '/' + (today.getMonth()+1) + '/' +today.getFullYear() + ' ' + today.toLocaleTimeString();
+    let date = new Date();
 
     let video = "";
     let img = "";
@@ -327,4 +440,13 @@ function editItem(e){
             displayData();
         }
     }
+}
+
+datePicker.onchange = function(e){
+    if (e.target.valueAsDate !== null){
+        displayDataByDate(e.target.valueAsDate);
+    } else {
+        displayData();
+    }
+    
 }
